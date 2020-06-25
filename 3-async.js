55cf5f25e094f8ac3c0a4ea737da42caa6bdd1db
TORESTO <therealtoresto@gmail.com>
2020-06-25 11:31:47 +0300

    async functions example

---
+++ 3-async.js
@@ -0,0 +1,47 @@
+'use strict';
+async function inc(a) {
+    return a + 1;
+}
+
+const sum = async function(a, b) {
+    return a + b; 
+};
+
+const max = async (a, b) => (a > b ? a : b);
+
+const avg = async (a, b) => {
+    const s = sum(a, b);
+    return s / 2;
+};
+
+const obj = {
+    name: 'Marcus Aurelius',
+    async split(sep = ' ') {
+        return await this.name.split(sep);
+    }
+};
+
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+    static async of(name) {
+        return await new Person(name);
+    }
+    async split(sep = ' ') {
+        return this.name.split(sep);
+    }
+}
+
+const person = Person.of('Marcus Aurelius');
+
+(async () => {
+
+    console.log('await inc(5) =', await inc(5));
+    console.log('await sum(1, 3) =', await sum(1, 3));
+    console.log('await max(8, 6) =', await max(8, 6));
+    console.log('await avg(8, 6) =', await avg(8, 6));
+    console.log('await obj.split() =', await person.split());
+    console.log('await person.split() =', await person.split());
+
+})();
